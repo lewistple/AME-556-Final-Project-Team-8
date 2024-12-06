9d4eed8cd1eeaac9e96a7267d53fb35e58c9c534
lewistple <tianpail@usc.edu>
2024-12-06 15:05:39 -0800

    Minor update

--- Task 2/AME556_Project_Task2_Setup.m
+++ Task 2/AME556_Project_Task2_Setup.m
@@ -1,6 +1,6 @@
 %% AME-556
 
-% HW4 Problem 2 (a. QP controller)
+% Project Task 2 Setup
 
 %% Clear
 
@@ -23,7 +23,6 @@ Il_zz = params.m * params.l^2 / 12;
 Ib_xx = params.mb * params.a^2 / 12;
 Ib_yy = params.mb * params.b^2 / 12;
 Ib_zz = params.mb * (params.a^2 + params.b^2) / 12;
-%params.Ib = diag([Ib_xx Ib_yy Ib_zz]);
 
 % Spatial contact force parameters
 Kp_g = 1e5;
@@ -31,3 +30,11 @@ Kd_g = 1e3;
  
 % Initial states
 params.q0 = [0; 0.45; 0; -pi/3; pi/2; -pi/6; pi/2];
+
+%% Plot
+
+plot(out.F.Time, squeeze(out.F.Data(1, :, :)), LineWidth=2); hold on; grid on;
+plot(out.F.Time, squeeze(out.F.Data(2, :, :)), LineWidth=2); 
+plot(out.F.Time, squeeze(out.F.Data(4, :, :)), LineWidth=2); 
+plot(out.F.Time, squeeze(out.F.Data(5, :, :)), LineWidth=2); 
+legend('F1x', 'F1y', 'F2x', 'F2y');
